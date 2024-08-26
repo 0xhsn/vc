b1782113ac846f925c1d6a2e93ba74b1b79ef5b2
macdoos <bigmacdoos@gmail.com>
2024-08-26 08:42:04 +0200

    feat: Get a recursive list of all files of a cloned repo

--- uploud/src/server.ts
+++ uploud/src/server.ts
@@ -1,22 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import simpleGit from 'simple-git';
-import { generate_deployment_id } from './utils';
+import express from "express";
+import cors from "cors";
+import simpleGit from "simple-git";
+import { generate_deployment_id, list_all_repo_files } from "./utils";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/deploy', async (req, res) => {
+app.post("/deploy", async (req, res) => {
   const repo_url = req.body.repo_url;
-  console.log('Repository URL: ', repo_url);
+  console.log("Repository URL: ", repo_url);
 
   const deployment_id = generate_deployment_id();
 
   await simpleGit().clone(repo_url, `./clones/${deployment_id}`);
 
+  list_all_repo_files(deployment_id).then((files) => {
+    console.log(`Files in the repository with id: ${deployment_id} `, files);
+  });
+
   res.json({
-    id: deployment_id
+    id: deployment_id,
   });
 });
 
--- uploud/src/utils.ts
+++ uploud/src/utils.ts
@@ -2,5 +2,29 @@ const MAX_ID_LENGTH = 5;
 
 export const generate_deployment_id = () => {
   return Math.random().toString(36).substr(2, MAX_ID_LENGTH);
-}
+};
 
+export const list_all_repo_files = async (deployment_id: string) => {
+  const fs = require("fs");
+  const path = require("path");
+
+  const directory = path.join(__dirname, `../clones/${deployment_id}`);
+
+  const walk = async (dir: string) => {
+    let results: string[] = [];
+    const list = await fs.promises.readdir(dir);
+    for (const file of list) {
+      const full_path = path.join(dir, file);
+      const stat = await fs.promises.stat(full_path);
+      if (stat && stat.isDirectory()) {
+        const res = await walk(full_path);
+        results = results.concat(res);
+      } else {
+        results.push(full_path);
+      }
+    }
+    return results;
+  };
+
+  return walk(directory);
+};
