c90303888a5d6f6060e7828ec61706f1023c8e0a
macdoos <bigmacdoos@gmail.com>
2024-08-28 00:27:36 +0200

    feat: Process SQS msgs on deploy server

--- deploy/src/server.ts
+++ deploy/src/server.ts
@@ -1,6 +1,18 @@
-console.log("Hello via Bun!");
-import { subscribe_to_sqs_message } from "./utils";
+console.log("Starting deploy server...");
+import { subscribe_to_sqs_message, download_s3_directory } from "./utils";
 
-while (true) {
-  await subscribe_to_sqs_message();
+async function processMessages() {
+  while (true) {
+    try {
+      const deployment_id: any = await subscribe_to_sqs_message();
+      
+      if (deployment_id) {
+        await download_s3_directory(deployment_id);
+      }
+    } catch (error) {
+      console.error("Error processing SQS message or downloading S3 directory:", error);
+    }
+  }
 }
+
+processMessages();
