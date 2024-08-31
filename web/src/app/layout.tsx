d8cb4d609d58532cc8ed12f4e085ede37127f235
0xhsn <mail@7asan.io>
2024-09-01 00:01:16 +0200

    ui: add dark mode + styles

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "./nav";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className="flex flex-col items-center justify-center p-10">
+              <div className="z-10 w-full max-w-xl flex flex-col items-center justify-center font-mono text-sm">
+              <Nav />
+              {children}
+              </div>
+            </main>
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
